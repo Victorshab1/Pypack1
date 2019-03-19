fae271719949719970032c0c731afebcc098387a
Victor Shabalala <Victor.S@explore-datascience.net>
2019-03-19 16:45:58 +0200

    second commit

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mypackage',
+    name='Pypack1',
     version='0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='EDSA example python package',
     long_description=open('README.md').read(),
     install_requires=['numpy'],
-    url='https://github.com/<username>/<package-name>',
-    author='<Your Name>',
-    author_email='<Your Email>'
+    url='https://github.com/Victorshab1/Pypack1',
+    author='Victorshab1',
+    author_email='victorshab@gmail.com'
 )
